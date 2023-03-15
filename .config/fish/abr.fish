4a40b395be51f4ffdf8436071c426b413847667a
SeniorMars <cjh16@rice.edu>
2023-03-15 17:53:07 -0400

    added fish

---
+++ .config/fish/abr.fish
@@ -0,0 +1,44 @@
+# these are abreviations for fish shell
+abbr -a ali '$EDITOR ~/.config/fish/abr.fish'
+abbr -a alr '$EDITOR ~/.config/alacritty/alacritty.yml'
+abbr -a cat ccat
+abbr -a grep 'grep --color=auto'
+abbr -a glp "git log --graph --pretty=format:'%C(yellow)%h%C(auto)%d%Creset %s %C(white)- %an, %ar%Creset'"
+abbr -a gla 'git log --all --graph --decorate --oneline'
+abbr -a muttrc 'cd ~/.config/mutt/;$EDITOR ~/.config/mutt/muttrc'
+abbr -a python python3
+abbr -a se 'fzf | xargs -r $EDITOR'
+abbr -a sv sudoedit
+abbr -a tiny 'tiny -c ~/.config/tiny/config.yml'
+abbr -a vim $EDITOR
+abbr -a vimrc '$EDITOR ~/.config/nvim/init.lua'
+abbr -a fishrc '$EDITOR ~/.config/fish/config.fish'
+
+# # Directory
+abbr -a md mkdir -p
+abbr -a rd rmdir
+
+# # List directory contents
+abbr -a ls exa
+abbr -a sl exa
+abbr -a la exa -aF
+abbr -a ll exa -al
+abbr -a l exa -a
+abbr -a l1 exa -1
+
+abbr -a q exit
+abbr -a yr 'cal -y'
+abbr -a mr 'neomutt'
+abbr -a c cargo
+abbr -a ct 'cargo t'
+abbr -a cb 'cargo b'
+abbr -a e nvim
+abbr -a m make
+abbr -a o open
+abbr -a g git
+abbr -a gc 'git checkout'
+abbr -a ga 'git add -p'
+abbr -a vimdiff 'nvim -d'
+abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
+abbr -a pr 'gh pr create -t (git show -s --format=%s HEAD) -b (git show -s --format=%B HEAD | tail -n+3)'
+abbr -a backup 'restic --repo /Volumes/backup/tetra_backup/ backup ~ --exclude-file=/Users/charlie/.config/rclone/exclude_backup'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,5 @@
-source ~/.config/profile/aliases
-abbr -a yr 'cal -y'
-abbr -a mr 'neomutt'
-abbr -a c cargo
-abbr -a e nvim
-abbr -a m make
-abbr -a o open
-abbr -a g git
-abbr -a gc 'git checkout'
-abbr -a ga 'git add -p'
-abbr -a vimdiff 'nvim -d'
-abbr -a ct 'cargo t'
-abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
-abbr -a pr 'gh pr create -t (git show -s --format=%s HEAD) -b (git show -s --format=%B HEAD | tail -n+3)'
-abbr -a backup 'restic --repo /Volumes/backup/tetra_backup/ backup ~ --exclude-file=/Users/karl/.config/rclone/exclude_backup'
-set FISH_CLIPBOARD_CMD "cat"
+function last_history_item; echo $history[1]; end
+abbr -a !! --position anywhere --function last_history_item
 
 # Man stuff
 setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
@@ -23,21 +9,18 @@ setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
 setenv LESS_TERMCAP_so \e'[01;33m'       # begin standout-mode - info box
 setenv LESS_TERMCAP_us \e'[04;38;5;10m'  # begin underline
 setenv LESS_TERMCAP_ue \e'[0m'           # end underline
-
+ 
 # fzf
 setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
 setenv FZF_ALT_C_COMMAND "$FZF_CTRL_T_COMMAND"
 setenv FZF_CTRL_T_COMMAND "fd -t d --hidden --follow --exclude \".git\" . $HOME"
 setenv FZF_DEFAULT_OPTS '--height 20%'
-
+ 
+set -gx SHELL /opt/homebrew/bin/fish
 set -g fish_key_bindings hybrid_bindings
 set fish_key_bindings fish_user_key_bindings
-if status is-interactive
-	if ! set -q TMUX
-		exec tmux
-	end
-end
-
-zoxide init fish | source
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
-test -e ~/.config/iterm2/iterm2_shell_integration.fish ; and source ~/.config/iterm2/iterm2_shell_integration.fish
+source ~/.config/fish/abr.fish
+zoxide init fish | source
+atuin init fish | source
+# set -x DBUS_SESSION_BUS_ADDRESS unix:path=(launchctl getenv DBUS_LAUNCHD_SESSION_BUS_SOCKET)
